4909c60dc56e09cc1c6406346510e4081f03f628
Anthony Shorter <anthshor@gmail.com>
2015-06-17 18:48:01 +1200

    Added variables and addUmask

--- Stage2/provision.sh
+++ Stage2/provision.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-# Proxy
-[ -f /vagrant/proxy.env ] && source /vagrant/proxy.env
+# Provisioning script for Oracle Grid and ASM
+# -------------------------------------------
+
+# define the global variables
+
+export STAGE="/u01/stage"
+export SOFTWARE="/u01/software"
+
+###################
+#
+# Functions
+#
+###################
 
 installPackages()
 {
-  echo "installing oracle-rdbms-server-12cR1-preinstall" 
-  #PACKAGES="oracle-rdbms-server-12cR1-preinstall.x86_64 xorg-x11-xauth.x86_64 xorg-x11-server-utils.x86_64 oracleasm-support.x86_64" 
+  echo "installing oracle pre-requirements" 
+  PACKAGES="oracle-rdbms-server-12cR1-preinstall.x86_64 xorg-x11-xauth.x86_64 xorg-x11-server-utils.x86_64 oracleasm-support.x86_64" 
+  rpm -q $PACKAGES 
+  if [ $? -ne 0 ]; then 
+    #yum clean all 
+    yum -y install $PACKAGES  
+  fi
 }
 
 createGroups()
 {
-  # grid and oracle user
-  #create the extra groups for db12c role separation
+  # create the extra groups for db12c role separation
   echo "Checking groups for grid and oracle user"
 
   grep ^asmdba:    /etc/group 2>&1 > /dev/null || groupadd -g 54318 asmdba
@@ -43,14 +58,16 @@ createUsers()
 unpackSoftware()
 {
   # Unpack previously downloaded software
-  mkdir -p /u01 /u02 /u03
-  mkdir -p /u01/stage
+  [ -d /u01 ] || mkdir /u01 
+  [ -d /u02 ] || mkdir /u02 
+  [ -d /u03 ] || mkdir /u03
+  [ -d ${STAGE} ] || mkdir ${STAGE}
 
-  pushd /u01/stage
-    unzip -n /u01/software/linuxamd64_12102_database_1of2.zip
-    unzip -n /u01/software/linuxamd64_12102_database_2of2.zip
-    unzip -n /u01/software/linuxamd64_12102_grid_1of2.zip
-    unzip -n /u01/software/linuxamd64_12102_grid_2of2.zip
+  pushd ${STAGE}
+    unzip -n ${SOFTWARE}/linuxamd64_12102_database_1of2.zip 
+    unzip -n ${SOFTWARE}/linuxamd64_12102_database_2of2.zip
+    unzip -n ${SOFTWARE}/linuxamd64_12102_grid_1of2.zip
+    unzip -n ${SOFTWARE}/linuxamd64_12102_grid_2of2.zip
   popd
 }
 
@@ -67,3 +84,28 @@ createDirectories()
   chown oracle:oinstall /u01/app/oracle
 
 }
+
+addUmask()
+{
+  grep "umask 022" /home/grid/.bash_profile 
+  if [ $? -ne 0 ]; then
+    su - grid -c 'echo "umask 022" > /tmp/umask; cat .bash_profile /tmp/umask > /tmp/profile; cat /tmp/profile > .bash_profile; rm /tmp/umask; rm /tmp/profile'
+  fi
+}
+
+
+##############
+#
+# Main
+#
+#############
+
+# Proxy
+[ -f /vagrant/proxy.env ] && source /vagrant/proxy.env
+
+installPackages
+createGroups
+createUsers
+unpackSoftware
+createDirectories
+addUmask
